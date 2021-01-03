01b582320f6b906ac1aae9e2f94e41c08cb4b02c
outstandingboy <outstandingboy@kakao.com>
2021-01-03 17:59:20 +0900

    Updates

---
+++ precache-manifest.aa4736139b66eec29624e632b17bf795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c3fe59a44b3cfb4c60fb453e4d4ff",
+    "url": "/barcode-gen2/index.html"
+  },
+  {
+    "revision": "3ecc5362eee048d58007",
+    "url": "/barcode-gen2/static/css/main.9fb09a97.chunk.css"
+  },
+  {
+    "revision": "108f2796c44280eeb581",
+    "url": "/barcode-gen2/static/js/2.75d9868d.chunk.js"
+  },
+  {
+    "revision": "9e2b7d758ddfb4253f5a03f5871c799c",
+    "url": "/barcode-gen2/static/js/2.75d9868d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecc5362eee048d58007",
+    "url": "/barcode-gen2/static/js/main.4bd2f3b9.chunk.js"
+  },
+  {
+    "revision": "31988802f865c05593f3",
+    "url": "/barcode-gen2/static/js/runtime-main.57273369.js"
+  }
+]);
